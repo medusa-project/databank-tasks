fc992e1eea5de1ad19c32dfcdb563fe142edb40b
Colleen Fallaw <mfall3@illlinois.edu>
2018-10-05 14:33:24 -0500

    refine deploy configuration

--- config/initializers/app.rb
+++ config/initializers/app.rb
@@ -1,3 +1,3 @@
-APP_CONFIG = YAML.load_file(ERB.new(File.join(Rails.root, 'config', 'app.yml')))
+APP_CONFIG = YAML.load_file(ERB.new(File.read(File.join(Rails.root, 'config', 'app.yml'))).result)
 
 Application.storage_manager = StorageManager.new

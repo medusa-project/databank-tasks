6ac3c160f15cef2ac7cbbdb10163f6fd423f68e1
Colleen Fallaw <mfall3@illlinois.edu>
2018-10-05 15:08:50 -0500

    troubleshoot deploy configuration

--- config/initializers/app.rb
+++ config/initializers/app.rb
@@ -1,3 +1,5 @@
+Rails.logger.warn("Rails.env: #{Rails.env}")
+
 APP_CONFIG = YAML.load(ERB.new(File.read(File.join(Rails.root, 'config', 'app.yml'))).result)
 
 Application.storage_manager = StorageManager.new

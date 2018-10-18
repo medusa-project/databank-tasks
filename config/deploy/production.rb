32a2e1140e23742f559386698e929adef50cbbbe
Colleen Fallaw <mfall3@illlinois.edu>
2018-10-18 14:22:47 -0500

    refine deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-set :rails_env, 'staging'
+set :rails_env, 'production'
 set :deploy_to, '/home/lib-medusa-databank'
 server 'rdf-prod.library.illinois.edu', user: 'lib-medusa-databank', roles: %w{app db web}
 
